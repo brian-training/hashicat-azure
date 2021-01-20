032ad4624b94eca0bb658d6e24539fded30aadd3
Brian <brian.generic1@gmail.com>
2021-01-20 20:57:47 +0000

    added vnet module

--- vnet.tf
+++ vnet.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/Staples-BS/network/azurerm"
   version = "3.0.1"
   # insert required variables here
-  resource_group_name = "training1"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
